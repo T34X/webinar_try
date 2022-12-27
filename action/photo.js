c4b9ec748cb842deda1821564d31680602c74dae
akumaru <akumaru.senju@gmail.com>
2022-12-27 16:29:04 +0500

    working with photos

--- action/photo.js
+++ action/photo.js
@@ -1,6 +1,10 @@
 const { bot } = require("../core/bot");
 
 bot.on('photo', ctx => {
+    
+    let photos = ctx.message.photo
+    let user_id = ctx.message.chat.id
+    
     ctx.replyWithHTML("<b>Qabul qilindi!</b>").then()
-    console.log(ctx.message.photo)
+    ctx.telegram.sendPhoto(user_id, photos[photos.length - 1].file_id).then()
 })
