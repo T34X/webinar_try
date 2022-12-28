e000f2cf94649946f5706e6d84f5dea6147df492
akumaru <akumaru.senju@gmail.com>
2022-12-28 21:56:52 +0500

    confession bot

--- confession/lib/keyboars.js
+++
@@ -1,9 +0,0 @@
-const { Markup } = require("telegraf")
-
-const keyboards = {
-    "start" : Markup.inlineKeyboard([Markup.button.callback("Buyruqlarni ko'rsatish!","help")])
-}
-
-module.exports = {
-    keyboards
-}
